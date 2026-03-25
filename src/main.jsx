ff786b74079af82132ff18ac7a34d65d6869c03b
aekae47 <aekae47@gmail.com>
2026-03-25 23:18:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // This is where you would paste your Tailwind @tailwind directives
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
